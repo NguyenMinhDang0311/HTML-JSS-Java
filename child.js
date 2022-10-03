812c65fa166b49d02f54bfbaebd50bdd01155899
Nguyễn Minh Đăng <111495387+NguyenMinhDang0311@users.noreply.github.com>
2022-10-03 16:45:27 +0700

    lession05

---
+++ child.js
@@ -0,0 +1,6 @@
+var startItem=document.getElementsByTagName('ul')[0];
+var firstItem=startItem.firstChild;
+var lassItem=startItem.lastChild;
+
+firstItem.className='complete';
+lassItem.className='cool';
